cd77a9a11a3557727b41fa64762513a0238822bf
mirbek <mamytalievmirbek2008@gmail.com>
2025-03-22 17:39:38 +0600

    first commit

---
+++ polls/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import TaskViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'Task', TaskViewSet, basename='Task')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
